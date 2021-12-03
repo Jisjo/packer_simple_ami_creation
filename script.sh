7c8e9d3d4925b24d2c0680f6d7e5562e694c1b80
Jisjo <61861646+Jisjo@users.noreply.github.com>
2021-12-03 11:56:58 +0530

    v2

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@ sudo yum install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
 
-sudo echo "This is test website from packer" >> /var/www/html/inde.html
+sudo echo "This is test website from packer" >> /var/www/html/index.html
 sudo chown apache. /var/www/html/ -R
